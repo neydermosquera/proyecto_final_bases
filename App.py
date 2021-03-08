2c1ff096576fc21e39271a9739253eba0bc23455
Neyder Mosquera <neyderfabianmosquera@gmail.com>
2021-03-08 16:10:57 -0500

    Update App.py

--- App.py
+++ App.py
@@ -40,30 +40,125 @@ def registro():
 def formpasajero():
     return render_template('formpasajero.html')
 
+@app.route('/formempleado.html')
+def formempleado():
+    return render_template('formempleado.html')
 
-@app.route('/registro.html/agregar_pasajero', methods=['POST'])
-def add_contact():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        apellido = request.form['apellido']
-        telefono = request.form['telefono']
-        print(nombre)
-        print(apellido)
-        print(telefono)
+@app.route('/formruta.html')
+def formruta():
+    return render_template('formruta.html')
+
+@app.route('/formpasajeVuelo.html')
+def formpasajeVuelo():
+    return render_template('formpasajeVuelo.html')
+
+@app.route('/formmarca.html')
+def formmarca():
+    return render_template('formmarca.html')
+
+@app.route('/formestadovuelo.html')
+def formestadoVuelo():
+    return render_template('formestadovuelo.html')
+
+@app.route('/formcargo.html')
+def formcargo():
+    return render_template('formcargo.html')
+
+@app.route('/formavion.html')
+def formavion():
+    return render_template('formavion.html')
+
+@app.route('/consultapasajero.html')
+def consultapasajero():
+    return render_template('consultapasajero.html')
+
+
+@app.route('/registro.html/agregar_marca', methods=['POST'])
+def add_marca():
+    if (request.method) == 'POST':
+        descripcion = request.form['descripcion']
+        print(descripcion)
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO contacts (nombre, apellido, telefono) VALUES (%s, %s, %s)', (nombre, apellido, telefono))
+        cur.callproc('agregarMarca', [descripcion])
         mysql.connection.commit()
-        flash('Contacto agregado satisfactoriamente')
-        return redirect(url_for('formularios'))
+        flash('Nueva MARCA agregada satisfactoriamente')
+        return redirect(url_for('formmarca'))
 
+@app.route('/registro.html/agregar_cargo', methods=['POST'])
+def add_cargo():
+    if (request.method) == 'POST':
+        descripcion = request.form['descripcion']
+        print(descripcion)
+        cur = mysql.connection.cursor()
+        cur.callproc('agregarCargo', [descripcion])
+        mysql.connection.commit()
+        flash('Nueva CARGO agregado satisfactoriamente')
+        return redirect(url_for('formcargo'))
 
-@app.route('/edit')
-def edit_contact():
-    return 'edit contact'
+@app.route('/resultadopasajero.html', methods=['POST'])
+def resultadoPasajero():
+    if request.method == 'POST':
+        documento = request.form['descripcion']
+        cur = mysql.connection.cursor()
+        cur.execute("""SELECT per.* FROM persona per, pasajero pas 
+                    WHERE per.numerodocumento = %s AND
+                    per.idpersona = pas.idpasajero """, (documento))
+        data = cur.fetchall()
+    return render_template('resultadopasajero.html', persona = data)
+
+@app.route('/consultamarca.html')
+def consultaMarca():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM marca')
+    data = cur.fetchall()
+    return render_template('consultamarca.html', marca = data)
+
+@app.route('/consultacargo.html')
+def consultaCargo():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM cargo')
+    data = cur.fetchall()
+    return render_template('consultaCargo.html', cargo = data)
+
+@app.route('/deletecargo/<string:id>')
+def deleteCargo(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM cargo WHERE idcargo = {0}'.format(id))
+    mysql.connection.commit()
+    flash('CARGO eliminado con éxito')
+    return redirect(url_for('consultaCargo'))
+
+@app.route('/editcargo/<id>')
+def getCargo(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM cargo WHERE idcargo = %s', (id))
+    data = cur.fetchall()
+    return render_template('editcargo.html', carg = data[0])
+
+@app.route('/updatecargo/<id>', methods=['POST'])
+def updateCargo(id):
+    if request.method == 'POST':
+        nombre = request.form['descripcion']
+    cur = mysql.connection.cursor()
+    cur.execute(""" 
+        UPDATE cargo
+        SET nombre = %s 
+        WHERE idcargo = %s
+    """, (nombre, id))
+    mysql.connection.commit()
+    flash('CARGO actualizado con éxito')
+    return redirect(url_for('consultaCargo'))
+   
 
 @app.route('/delete/<string:id>')
-def delete_contact(id):
+def delete_cargo(id):
     return id
+
+def mostrarPaises():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT nombre FROM pais')
+    paises = cur.fetchall()
+    print(paises)
 
 
 if __name__ == '__main__':
